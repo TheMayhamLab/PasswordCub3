52408a3e21bcd2d8d91cb5f5c362c194a8017ad8
The Mayham Lab <michael.vieau@gmail.com>
2019-09-27 10:42:03 -0500

    Delete new-git-repo.sh

--- new-git-repo.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter repo name: "; read NAME
-
-git init
-git add .
-git commit -m "First commit"
-git remote add origin https://github.com/TheMayhamLab/${NAME}.git
-git remote -v
-git push origin master
-git commit
